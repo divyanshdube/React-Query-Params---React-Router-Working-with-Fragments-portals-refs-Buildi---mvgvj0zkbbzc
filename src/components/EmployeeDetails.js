e15c23711c660c9275ef9b6d5925ff91bad39169
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-10-15 15:53:27 +0530

    Update EmployeeDetails.js

--- src/components/EmployeeDetails.js
+++ src/components/EmployeeDetails.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Outlet, useSearchParams } from "react-router-dom";
 
 const EmployeeDetails = () =>{
-    const [searchParams, setSearchParams] = useSearchParams();
-    
-    const id = searchParams.get("id");
-    const name = searchParams.get("name");
-    const designation = searchParams.get("designation");
-    
-    return(
-        <div className= "details">
-            Employee {id} named {name} work as {designation}
-            </div>
-    )
-    
-}
+    let [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  const name = searchParams.get("name");
+  const designation = searchParams.get("designation");
+  return (
+    <>
+      <div className="details">
+        Employee {id} named {name} works as {designation}
+      </div>
+      <Outlet />
+    </>
+  );
+  //Write your code here
+};
 
 export default EmployeeDetails;
